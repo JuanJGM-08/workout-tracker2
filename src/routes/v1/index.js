3ccf66d09093f38383ab8e69f328f4eae644bebc
Su nombre <uiehcskuf@gmail.com>
2025-10-02 23:29:06 -0500

    Se creo y se completo la rama "feat/workoutSchedule" con sus cruds completados

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const usersRoutes = require('./users.routes');
 const exercisesRoutes = require('./exercises.routes');
 const workoutPlansRoutes = require('./workoutPlans.routes');
-const workoutExercisesRoutes = require('./workoutExercises.routes'); 
+const workoutExercisesRoutes = require('./workoutExercises.routes');
+const workoutScheduleRoutes = require('./workoutSchedule.routes'); 
 
 router.use('/users', usersRoutes);
 router.use('/exercises', exercisesRoutes);
 router.use('/workoutPlans', workoutPlansRoutes);
-router.use('/workoutExercises', workoutExercisesRoutes); 
+router.use('/workoutExercises', workoutExercisesRoutes);
+router.use('/workoutSchedules', workoutScheduleRoutes); 
 
 module.exports = router;
